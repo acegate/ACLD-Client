fb834f1796433e38a736a57f27d692ffbdec180b
kimhyunso <gustn9515@gmail.com>
2023-06-09 16:56:07 +0900

    20230609 16:56

--- util.py
+++ util.py
@@ -5,14 +5,21 @@ import re, uuid
 import socket
 from tkinter import *
 import pyautogui
+import cv2
+import numpy as np
 
 class Util:
     def __init__(self):
         window = Tk()
+        # self.WINDOW_WIDTH = 720
+        # self.WINDOW_HEIGHT = 720
+        # self.X = window.winfo_screenwidth() // 2 - self.WINDOW_WIDTH // 2
+        # self.Y = window.winfo_screenheight() // 2 - self.WINDOW_HEIGHT // 2
+
+        self.WINDOW_WIDTH = window.winfo_screenwidth() 
+        self.WINDOW_HEIGHT = window.winfo_screenheight()
         self.X = 0
         self.Y = 0
-        self.WINDOW_WIDTH = window.winfo_screenwidth()
-        self.WINDOW_HEIGHT = window.winfo_screenheight()
         self.create_cominfo_to_json()
 
     def create_cominfo_to_json(self) -> dict:
@@ -25,8 +32,9 @@ class Util:
         return temp
 
     def screen_shot(self) -> Image:
-        return pyautogui.screenshot(region=(self.X, self.Y, self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
-        
+        screenshot = pyautogui.screenshot(region=(self.X, self.Y, self.WINDOW_WIDTH, self.WINDOW_HEIGHT))
+        return cv2.cvtColor(np.array(screenshot), cv2.COLOR_RGB2BGR)
+
 
     def get_rootpath(self):
         return self.__path
